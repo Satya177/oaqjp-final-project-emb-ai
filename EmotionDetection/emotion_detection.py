183beaca26fd775a83ee805bac55e27ba93525ad
Satya177 <Pakpols3425@gmail.com>
2026-04-20 11:54:23 -0400

    Final submission: Adding server and test files with error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,6 +2,17 @@ import requests
 import json
 
 def emotion_detector(text_to_analyze):
+    # Logika jika user tidak ngetik apa-apa (PENTING!)
+    if not text_to_analyze.strip():
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
@@ -12,13 +23,14 @@ def emotion_detector(text_to_analyze):
         formatted_response = json.loads(response.text)
         emotions = formatted_response['emotionPredictions'][0]['emotion']
         dominant_emotion = max(emotions, key=emotions.get)
-        
         return {
-            'anger': emotions['anger'],
-            'disgust': emotions['disgust'],
-            'fear': emotions['fear'],
-            'joy': emotions['joy'],
-            'sadness': emotions['sadness'],
-            'dominant_emotion': dominant_emotion
+            'anger': emotions['anger'], 'disgust': emotions['disgust'],
+            'fear': emotions['fear'], 'joy': emotions['joy'],
+            'sadness': emotions['sadness'], 'dominant_emotion': dominant_emotion
         }
-    return None
+    
+    # Jika API error (status 400)
+    return {
+        'anger': None, 'disgust': None, 'fear': None,
+        'joy': None, 'sadness': None, 'dominant_emotion': None
+    }
